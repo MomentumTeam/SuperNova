198c79350d50b6a255a7ff32515971da078e4fbf
Barak Steindl <steindl.barak@gmail.com>
2021-06-14 18:07:14 +0300

    feat: proto file of kartoffel-service

---
+++ kartoffel-service/src/server.ts
@@ -0,0 +1,80 @@
+import path from "path";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import * as C from "./config";
+import { logger } from "./logger";
+
+const PROTO_PATH = __dirname.includes("dist")
+  ? path.join(__dirname, "../../proto/spikeService.proto")
+  : path.join(__dirname, "../proto/spikeService.proto");
+
+export class Server {
+  private server: grpc.Server;
+  constructor() {
+    this.server = new grpc.Server();
+    this.initServer();
+  }
+
+  private getProtoDescriptor() {
+    try {
+      const packageDefinition: protoLoader.PackageDefinition =
+        protoLoader.loadSync(PROTO_PATH, {
+          keepCase: true,
+          longs: String,
+          enums: String,
+          defaults: true,
+          oneofs: true,
+        });
+      const protoDescriptor: grpc.GrpcObject =
+        grpc.loadPackageDefinition(packageDefinition);
+      const spikeServiceDescriptor: any = protoDescriptor.Spike;
+      return spikeServiceDescriptor;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  initServer() {
+    try {
+      const spikeServiceDescriptor: any = this.getProtoDescriptor();
+      logger.log({
+        level: "info",
+        message: `Proto was loaded successfully from file: ${PROTO_PATH}`,
+        label: "initServer",
+      });
+      this.server.addService(spikeServiceDescriptor.Spike.service, {});
+      logger.log({
+        level: "info",
+        message: `Grpc services were successfully added to the server`,
+        label: "initServer",
+      });
+    } catch (error) {
+      logger.log({
+        level: "error",
+        message: `Error while initializing the server: ${error.message}`,
+        label: "initServer",
+      });
+    }
+  }
+
+  async startServer(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.server.bindAsync(
+        `${C.host}:${C.port}`,
+        grpc.ServerCredentials.createInsecure(),
+        (bindError) => {
+          if (bindError) {
+            reject(bindError);
+          } else {
+            try {
+              this.server.start();
+              resolve();
+            } catch (startError) {
+              reject(startError);
+            }
+          }
+        }
+      );
+    });
+  }
+}
