cff9f2f06f8815fed93f34c919da977dea6898d9
liorayacob <liorayacob10@gmail.com>
2021-07-04 18:52:15 +0300

    refactor: added all request-service functions to Api-Gateway

--- api-gateway/src/requests/requests.controller.ts
+++ api-gateway/src/requests/requests.controller.ts
@@ -3,7 +3,7 @@ import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from "@grpc/proto-loader";
 import path from "path";
 import * as config from "../config";
-import { Request as RequestS, RequestArray} from "../interfaces/protoc/proto/requestService";
+import { Request as RequestS, RequestArray, SuccessMessage, CreateOGRes, CreateRoleRes, CreateEntityRes, AssignRoleToEntityRes, RenameOGRes, EditEntityRes, DeleteOGRes, DeleteRoleRes, DisconectRoleFromEntityRes, } from "../interfaces/protoc/proto/requestService";
 
 const PROTO_PATH = __dirname.includes("dist")
   ? path.join(__dirname, "../../proto/requestService.proto")
@@ -99,6 +99,138 @@ export default class RequestsController {
         const stageStatus: StageStatus = (<any>StageStatus)[status];
 
         requestsClient.UpdateADStatus({ requestId: req.body.requestId, status: stageStatus, message: req.body.message }, (err: any, response: RequestS) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async updateRequest(req: Request, res: Response) {
+        console.log('UpdateRequest')
+
+        requestsClient.UpdateRequest(req.body, (err: any, response: RequestS) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async renameOGRequest(req: Request, res: Response) {
+        console.log('RenameOGRequest')
+
+        requestsClient.RenameOGRequest(req.body, (err: any, response: RenameOGRes) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async renameRoleRequest(req: Request, res: Response) {
+        console.log('RenameRoleRequest')
+
+        requestsClient.RenameRoleRequest(req.body, (err: any, response: EditEntityRes) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async createOGRequest(req: Request, res: Response) {
+        console.log('createOGRequest')
+
+        requestsClient.CreateOGRequest(req.body, (err: any, response: CreateOGRes) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async createRoleRequest(req: Request, res: Response) {
+        console.log('createRoleRequest')
+
+        requestsClient.CreateRoleRequest(req.body, (err: any, response: CreateRoleRes) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async createEntityRequest(req: Request, res: Response) {
+        console.log('CreateEntityRequest')
+
+        requestsClient.CreateEntityRequest(req.body, (err: any, response: CreateEntityRes) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async assignRoleToEntityRequest(req: Request, res: Response) {
+        console.log('AssignRoleToEntityRequest')
+
+        requestsClient.AssignRoleToEntityRequest(req.body, (err: any, response: AssignRoleToEntityRes) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async editEntityRequest(req: Request, res: Response) {
+        console.log('EditEntityRequest')
+
+        requestsClient.EditEntityRequest(req.body, (err: any, response: EditEntityRes) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async disconectRoleFromEntityRequest(req: Request, res: Response) {
+        console.log('DisconectRoleFromEntityRequest')
+
+        requestsClient.DisconectRoleFromEntityRequest(req.body, (err: any, response: DisconectRoleFromEntityRes) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async deleteRoleRequest(req: Request, res: Response) {
+        console.log('DeleteRoleRequest')
+
+        requestsClient.DeleteRoleRequest(req.body, (err: any, response: DeleteRoleRes) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async deleteOGRequest(req: Request, res: Response) {
+        console.log('DeleteOGRequest')
+
+        requestsClient.DeleteOGRequest(req.body, (err: any, response: DeleteOGRes) => {
+            if (err) {
+                res.send(err);
+            }
+            res.send(response);
+          });
+    }
+
+    static async deleteRequest(req: Request, res: Response) {
+        console.log('DeleteRequest')
+
+        requestsClient.DeleteRequest(req.body, (err: any, response: SuccessMessage) => {
             if (err) {
                 res.send(err);
             }
