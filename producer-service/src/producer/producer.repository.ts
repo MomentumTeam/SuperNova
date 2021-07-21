b6eab831e20e1aa058b46c9957ec711adc646418
Barak Steindl <steindl.barak@gmail.com>
2021-07-21 12:53:20 +0300

    Changes to RS

--- producer-service/src/producer/producer.repository.ts
+++ producer-service/src/producer/producer.repository.ts
@@ -1,19 +1,46 @@
-import axios from "axios";
-import * as C from "../config";
+import axios from 'axios';
+import * as C from '../config';
 export class ProducerRepository {
   async pushIntoKartoffelQueue(message: any) {
-    console.log('pushIntoKartoffelQueue',message )
-    try {
-      await axios.post(`${C.queueApi}/kartoffelQueue`, message);
-    } catch (error) {
-      throw error;
+    console.log('pushIntoKartoffelQueue');
+    if (C.devMode) {
+      console.log(message);
+    } else {
+      return new Promise((resolve, reject) => {
+        try {
+          axios
+            .post(`${C.queueApi}/kartoffelQueue`, message)
+            .then(() => {
+              resolve(true);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        } catch (error) {
+          throw error;
+        }
+      });
     }
   }
   async pushIntoADQueue(message: any) {
-    try {
-      await axios.post(`${C.queueApi}/kartoffelQueue`, message);
-    } catch (error) {
-      throw error;
+    console.log('pushIntoADQueue');
+    if (C.devMode) {
+      console.log(message);
+    } else {
+      return new Promise((resolve, reject) => {
+        try {
+          axios
+            .post(`${C.queueApi}/adQueue`, message)
+            .then(() => {
+              resolve(true);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        } catch (error) {
+          throw error;
+        }
+      });
     }
   }
 }
