7ec4bf1ce782f38725726f79a729062302e7e3cb
liorayacob <liorayacob10@gmail.com>
2021-10-03 12:20:09 +0300

    fix: WLS_CRON_JOB

--- whiteListSync-script/src/config.ts
+++ whiteListSync-script/src/config.ts
@@ -1,7 +1,7 @@
 export const approverServiceUrl = process.env.WLS_APS_URL || '0.0.0.0:8085';
 export const hour = process.env.WLS_HOUR || `0`; //   0-23
 export const minute = process.env.WLS_MINUTE || `0`; //  0-59
-export const cronJob = process.env.EXS_CRON_JOB
+export const cronJob = process.env.WLS_CRON_JOB
   ? process.env.WLS_CRON_JOB === 'true'
     ? true
     : false
