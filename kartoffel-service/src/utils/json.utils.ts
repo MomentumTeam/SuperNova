af32926f86163b4c7870f3269a9fb3ba65a2df36
Barak Steindl <steindl.barak@gmail.com>
2021-10-27 17:32:44 +0300

    print urls KS

--- kartoffel-service/src/utils/json.utils.ts
+++ kartoffel-service/src/utils/json.utils.ts
@@ -1,6 +1,5 @@
 export function cleanUnderscoreFields(document: any): void {
   let keys: any = Object.keys(document);
-
   for (let key of keys) {
     if (key.startsWith('_')) {
       delete document[key];
