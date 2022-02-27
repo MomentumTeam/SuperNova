83667f098d810ea19b7ebe92e72de094202e0fa7
nettashviro <netta.zhviro@gmail.com>
2022-02-27 15:26:58 +0200

    fix

--- spike-service/src/index.ts
+++ spike-service/src/index.ts
@@ -1,5 +1,6 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import { findPath } from './utils/path';
+const grpcHealth = require("grpc-js-health-check");
 
 if (process.env.NODE_ENV !== 'production') {
   const ENV_PATH = findPath('supernova.env');
@@ -10,11 +11,14 @@ if (process.env.NODE_ENV !== 'production') {
 
 import { Server } from './server';
 import { logger } from './logger';
+import { setHealthStatus } from './health';
 
 async function main() {
   try {
     const server: Server = new Server();
     await server.startServer();
+    setHealthStatus(grpcHealth.servingStatus.SERVING);
+
     logger.info('spike-service started successfully');
   } catch (error: any) {
     logger.error(`Error while trying to start spike-service: ${error.message}`);
