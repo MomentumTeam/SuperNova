8b387f2c809c442c4632ad1575d774035558fa5b
liorayacob <liorayacob10@gmail.com>
2022-05-22 11:52:43 +0300

    fix: picture name

--- mail-service/src/utils/mailer.ts
+++ mail-service/src/utils/mailer.ts
@@ -24,7 +24,7 @@ const images = [
   },
   {
     filename: 'sapirLogo.svg',
-    path: path.join(`${C.localMailImagesFullPath}/sapirL.svg`),
+    path: path.join(`${C.localMailImagesFullPath}/sapirLogo.svg`),
     cid: 'sapirLogo@cid',
   },
 ];
